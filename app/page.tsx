34eef5ff88b5b3b9c781d6821627baf0cb1027ef
rjfuhrman42 <reidjf77@gmail.com>
2024-03-27 17:15:17 -0400

    wrap up styling, dates

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import DashBoard from "./dashboard/page";
 
 export default function Home() {
   return (
-    <main className="bg-white flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="bg-white flex min-h-screen flex-col items-center justify-center">
       <DashBoard />
     </main>
   );
--- components/RecipeViewer.tsx
+++ components/RecipeViewer.tsx
@@ -16,6 +16,7 @@ type Props = {
     Todos:
     [] Handle when text contains character references like &#39; and &amp;
     [] Don't show properties that are not provided (Author, Prep, Cook, etc.)
+    [] Consider adding a custom number icon for recipe steps
 
 */
 
@@ -53,17 +54,31 @@ function RecipeViewer({ recipe }: Props) {
   return (
     <>
       {recipe ? (
-        <div className="pt-20 max-w-[875px]">
-          <h1 className="text-3xl my-4">{name?.toString()}</h1>
-          <div className="flex flex-row h-[400px]">
+        <div className="flex flex-col gap-y-12 pt-20 w-full lg:max-w-[875px]">
+          <h1 className="text-3xl font-bold text-center">{name?.toString()}</h1>
+          <div className="flex flex-col md:flex-row h-[400px]">
             <div className="bg-cookeri-green-light p-8 flex-1">
-              <div className="pb-12">
+              <div className="flex flex-col pb-8 gap-y-2">
                 {authorDetails?.name && (
-                  <h3 className="">Author: {authorDetails?.name}</h3>
+                  <div className="flex flex-row">
+                    <p className="font-bold mr-1">Author: </p>
+                    <p>{authorDetails?.name}</p>
+                  </div>
                 )}
-                <h3 className="">Prep: {prep}</h3>
-                <h3 className="">Cook: {cook}</h3>
-                <h3 className="">Total: {total}</h3>
+                <div className="flex flex-row gap-x-2">
+                  <div className="flex flex-row">
+                    <p className="font-bold mr-1">Prep: </p>
+                    <p>{prep}</p>
+                  </div>
+                  <div className="flex flex-row">
+                    <p className="font-bold mr-1">Cook: </p>
+                    <p>{cook}</p>
+                  </div>
+                </div>
+                <div className="flex flex-row">
+                  <p className="font-bold mr-1">Total: </p>
+                  <p>{total}</p>
+                </div>
               </div>
               <p>{description?.toString()}</p>
             </div>
@@ -78,12 +93,12 @@ function RecipeViewer({ recipe }: Props) {
               </div>
             )}
           </div>
-          <div className="flex flex-row gap-x-8">
+          <div className="flex flex-col md:flex-row gap-x-8">
             <div className="flex flex-col flex-1">
               <h2 className="text-2xl my-4 font-bold">Ingredients</h2>
               <ul>
                 {ingredients?.map((ingredient, index) => (
-                  <li className="py-1" key={index}>
+                  <li className="py-2" key={index}>
                     {ingredient}
                   </li>
                 ))}
@@ -92,7 +107,7 @@ function RecipeViewer({ recipe }: Props) {
             <div className="flex flex-col flex-1">
               <h2 className="text-2xl my-4 font-bold">Steps</h2>
               {howToSteps && (
-                <ol className="list-decimal" type="1">
+                <ol className="list-decimal ml-4" type="1">
                   {howToSteps?.map((step, index) => {
                     return (
                       <li className="py-2" key={index}>
@@ -109,7 +124,7 @@ function RecipeViewer({ recipe }: Props) {
                     <h3 className="text-lg my-4 font-medium">
                       {String(section.name)}
                     </h3>
-                    <ol className="list-decimal" type="1">
+                    <ol className="list-decimal ml-4" type="1">
                       {itemArray.map((item, index) => {
                         return (
                           <li className="py-2" key={index}>
