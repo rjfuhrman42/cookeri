1faa582302469bc63e7cb50235ca3a25597615ac
rjfuhrman42 <reidjf77@gmail.com>
2024-03-26 22:30:58 -0400

    chore: add dayjs, parse durations
    wrap up styling, dates

--- components/RecipeViewer.tsx
+++ components/RecipeViewer.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import { Recipe, HowToStep, HowToSection } from "schema-dts";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 type Props = {
   recipe: Recipe | undefined;
@@ -8,10 +13,12 @@ type Props = {
 
 /*
 
-    Todos for later:
+    Todos:
     [] Handle when text contains character references like &#39; and &amp;
+    [] Don't show properties that are not provided (Author, Prep, Cook, etc.)
+    [] Consider adding a custom number icon for recipe steps
 
-  */
+*/
 
 function RecipeViewer({ recipe }: Props) {
   const {
@@ -40,20 +47,38 @@ function RecipeViewer({ recipe }: Props) {
 
   const authorDetails = author as { name: string } | undefined;
 
-  console.log(Date.parse(prepTime as string));
+  const prep = dayjs.duration(prepTime as string).humanize();
+  const cook = dayjs.duration(cookTime as string).humanize();
+  const total = dayjs.duration(totalTime as string).humanize();
 
   return (
     <>
       {recipe ? (
-        <div className="pt-20 max-w-[875px]">
-          <h1 className="text-3xl my-4">{name?.toString()}</h1>
-          <div className="flex flex-row h-[400px]">
+        <div className="flex flex-col gap-y-12 pt-20 w-full lg:max-w-[875px]">
+          <h1 className="text-3xl font-bold text-center">{name?.toString()}</h1>
+          <div className="flex flex-col md:flex-row h-[400px]">
             <div className="bg-cookeri-green-light p-8 flex-1">
-              <div className="pb-12">
-                <h3 className="">Author: {authorDetails?.name}</h3>
-                <h3 className="">Prep: {prepTime}</h3>
-                <h3 className="">Cook: {cookTime}</h3>
-                <h3 className="">Total: {totalTime}</h3>
+              <div className="flex flex-col pb-8 gap-y-2">
+                {authorDetails?.name && (
+                  <div className="flex flex-row">
+                    <p className="font-bold mr-1">Author: </p>
+                    <p>{authorDetails?.name}</p>
+                  </div>
+                )}
+                <div className="flex flex-row gap-x-2">
+                  <div className="flex flex-row">
+                    <p className="font-bold mr-1">Prep: </p>
+                    <p>{prep}</p>
+                  </div>
+                  <div className="flex flex-row">
+                    <p className="font-bold mr-1">Cook: </p>
+                    <p>{cook}</p>
+                  </div>
+                </div>
+                <div className="flex flex-row">
+                  <p className="font-bold mr-1">Total: </p>
+                  <p>{total}</p>
+                </div>
               </div>
               <p>{description?.toString()}</p>
             </div>
@@ -68,12 +93,12 @@ function RecipeViewer({ recipe }: Props) {
               </div>
             )}
           </div>
-          <div className="flex flex-row gap-x-8">
+          <div className="flex flex-col md:flex-row gap-x-8">
             <div className="flex flex-col flex-1">
               <h2 className="text-2xl my-4 font-bold">Ingredients</h2>
               <ul>
                 {ingredients?.map((ingredient, index) => (
-                  <li className="py-1" key={index}>
+                  <li className="py-2" key={index}>
                     {ingredient}
                   </li>
                 ))}
@@ -82,7 +107,7 @@ function RecipeViewer({ recipe }: Props) {
             <div className="flex flex-col flex-1">
               <h2 className="text-2xl my-4 font-bold">Steps</h2>
               {howToSteps && (
-                <ol className="list-decimal" type="1">
+                <ol className="list-decimal ml-4" type="1">
                   {howToSteps?.map((step, index) => {
                     return (
                       <li className="py-2" key={index}>
@@ -99,7 +124,7 @@ function RecipeViewer({ recipe }: Props) {
                     <h3 className="text-lg my-4 font-medium">
                       {String(section.name)}
                     </h3>
-                    <ol className="list-decimal" type="1">
+                    <ol className="list-decimal ml-4" type="1">
                       {itemArray.map((item, index) => {
                         return (
                           <li className="py-2" key={index}>
