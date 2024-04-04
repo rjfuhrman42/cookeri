df821f33b40d767637ca574ce1af110c197f8ff4
rjfuhrman42 <reidjf77@gmail.com>
2024-04-04 15:01:39 -0400

    chore: add in recipe yield, remove update title button

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -13,14 +13,6 @@ interface Props {
 }
 
 function SideBar({ children, recipe, setData }: Props) {
-  const [title, setTitle] = useState(recipe?.name?.toString());
-
-  useEffect(() => {
-    if (recipe?.name?.toString()) {
-      setTitle(recipe?.name?.toString());
-    }
-  }, [recipe]);
-
   return (
     <div className="flex flex-col">
       <div className="flex flex-col h-lvh gap-y-8 w-[350px] 2xl:w-[450px] p-6 2xl:p-8">
@@ -37,26 +29,15 @@ function SideBar({ children, recipe, setData }: Props) {
           </label>
           <Input
             type="text"
-            value={title}
+            value={recipe?.name?.toString()}
             color="default"
             size="lg"
             onChange={(e) => {
-              setTitle(e.target.value);
+              setData({ ...recipe, name: e.target.value, "@type": "Recipe" });
             }}
             className="w-full"
             name="recipe-title"
           />
-          <Button
-            className="font-league-spartan text-lg text-white w-full px-4"
-            onClick={() => {
-              setData({ ...recipe, name: title });
-            }}
-            size="md"
-            color="success"
-            endContent={<SaveIcon stroke="white" />}
-          >
-            Update title
-          </Button>
         </div>
         <div className="w-full flex flex-col gap-4 z-10 items-center justify-center">
           <p className="font-league-spartan text-lg text-left w-full pl-2">
