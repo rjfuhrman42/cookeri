a39676fc759528665605e7fa4bb23c936fabfb63
rjfuhrman42 <reidjf77@gmail.com>
2024-06-20 15:03:37 -0400

    feat: footer, privacy policy

--- app/page.tsx
+++ app/page.tsx
@@ -23,11 +23,11 @@ export default function Home() {
 
   return (
     <NextUIProvider>
-      <main className="flex flex-col items-center justify-start">
+      <main className="flex flex-col items-center justify-start overflow-x-hidden">
         <Navbar isUserLoggedIn={user !== null} color="cookeri-green-light" />
         <div className="flex flex-col items-center w-screen md:-mt-16">
           <Hero />
-          <section className="w-full flex flex-col items-center py-24 bg-cookeri-green-light overflow-x-hidden">
+          <section className="w-full flex flex-col items-center py-48 bg-cookeri-green-light overflow-x-hidden">
             <div className="gap-y-36 container flex flex-col">
               <FeatureBlock
                 title="Import"
@@ -42,6 +42,42 @@ export default function Home() {
               />
             </div>
           </section>
+          <footer className="bg-light-black h-[300px] w-screen flex flex-col items-center py-8">
+            <div className="container flex flex-col items-center justify-between h-full">
+              <h1 className="text-cookeri-green text-4xl font-gluten font-bold">
+                Cookeri
+              </h1>
+              <div className="flex gap-x-4">
+                <a
+                  href="mailto:hey@reidfuhrman.com"
+                  className="uppercase text-sm font-league-spartan text-lighter-grey"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Contact
+                </a>
+                <a
+                  href="/privacy-policy.pdf"
+                  className="uppercase text-sm font-league-spartan text-lighter-grey"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Privacy Policy
+                </a>
+              </div>
+              <p className="font-league-spartan text-base text-lighter-grey">
+                © {`${new Date(Date.now()).getUTCFullYear()} `}Made with 💚 by{" "}
+                <a
+                  href="https://cliki.in/lxnhe5asne"
+                  className="font-league-spartan text-base text-cookeri-green "
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Reid Fuhrman
+                </a>
+              </p>
+            </div>
+          </footer>
         </div>
       </main>
     </NextUIProvider>
