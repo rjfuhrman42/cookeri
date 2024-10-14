54390ed13111c1d64e31c9a5150958e367082afc
rjfuhrman42 <reidjf77@gmail.com>
2024-10-14 17:54:39 -0400

    fix: sidebar width overflow, max height on list

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 function SideBar({ children }: Props) {
   return (
-    <div className="fixed z-20 flex flex-col h-dvh bg-light-black text-white md:relative">
-      <div className="flex flex-col h-full gap-y-8 w-[250px] lg:w-[350px] 2xl:w-[450px] p-4 lg:p-6 2xl:p-8 mb-[4.5rem]">
+    <div className="h-full z-20 flex flex-col bg-light-black text-white relative">
+      <div className="flex flex-col flex-1 justify-start gap-y-8 w-[250px] lg:w-[350px] 2xl:w-[450px] p-4 lg:p-6 2xl:p-8">
         {children}
       </div>
     </div>
