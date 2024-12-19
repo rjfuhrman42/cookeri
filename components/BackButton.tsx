1cf2abda185586fed366b973c5e52f4a100ecd3b
rjfuhrman42 <reidjf77@gmail.com>
2024-12-19 11:09:11 -0500

    chore: re-add edit page

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -9,16 +9,25 @@ interface Props {
   text: string;
   noIcon?: boolean;
   className?: string;
+  pathname?: string;
 }
 
-function BackButton({ text, noIcon = false, className = "" }: Props) {
+function BackButton({
+  text,
+  noIcon = false,
+  className = "",
+  pathname = "",
+}: Props) {
   const router = useRouter();
   return (
     <Button
       className={className}
       startContent={noIcon ? <></> : <ArrowLeftIcon />}
       onPress={() => {
-        router.back();
+        if (pathname === "") {
+          router.back();
+        }
+        router.push(pathname);
       }}
       radius="sm"
       size="lg"
